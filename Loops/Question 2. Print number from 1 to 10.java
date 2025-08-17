6431382847d15e4c65c1afa1b2161e02276b189c
Aman Singh <aman60395@gmail.com>
2025-08-17 11:02:08 +0530

    Question 2. Print number from 1 to 10.java

---
+++ Loops/Question 2. Print number from 1 to 10.java
@@ -0,0 +1,10 @@
+//Print number from 1 to 10
+public class Main {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num <=10){
+            System.out.print(num+" ");
+            num++;
+        }
+    }
+}
