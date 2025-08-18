e61769ee227177f3247037c43212b5bbb6e6c22e
Aman Singh <aman60395@gmail.com>
2025-08-18 13:25:56 +0530

    Question 12. Continue keyword example.java

---
+++ Loops/Question 12. Continue keyword example.java
@@ -0,0 +1,14 @@
+//Continue keyword example
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       for(int i=1; i<=5; i++){
+           if(i==3){
+           continue;
+       }
+       System.out.println(i);
+    }
+    }
+}
