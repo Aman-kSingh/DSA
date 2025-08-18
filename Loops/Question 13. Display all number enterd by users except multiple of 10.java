dac2a63c88bb34384110c46be891cf87d6625ecc
Aman Singh <aman60395@gmail.com>
2025-08-18 13:46:00 +0530

    Question 13. Display all number enterd by users except multiple of 10.java

---
+++ Loops/Question 13. Display all number enterd by users except multiple of 10.java
@@ -0,0 +1,17 @@
+//Display all number enterd by users except multiple of 10
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        do{
+            System.out.println("Enter your number: ");
+         int n=sc.nextInt();
+         
+           if(n %10 ==0){
+           continue;
+       }
+       System.out.println(n);
+    }while(true);
+    }
+}
